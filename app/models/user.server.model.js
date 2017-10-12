1e1a92bbb2b7dceb2b43c5738991a68bdfcf1284
enochdone <enochdone@gmail.com>
2017-10-12 14:51:05 +1100

    Update user.server.model.js

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -30,6 +30,10 @@ var UserSchema = new Schema({
 });
             UserSchema.virtual('fullName').get(function() {
           return this.firstName + ' ' + this.lastName;
+        }).set(function(fullName){
+          var splitName = fullName.split(' ');
+          this.firstName = splitName[0] || '';
+          this.lastName = splitName[1] || '';
         });
 
             UserSchema.set('toJSON', {getters: true, virtuals: true});
